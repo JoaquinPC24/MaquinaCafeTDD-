bb6f8acc74ecc0e2478ce22f20683bf3854a7a45
Jack sparrow <124702598+JoaquinPC24@users.noreply.github.com>
2026-06-22 03:20:42 -0400

    test(tc-all): keep all 8 tests in RED (initial failing tests)

---
+++ MaquinaCafe/MaquinaCafe.cs
@@ -0,0 +1,44 @@
+﻿namespace MaquinaCafe;
+
+using System.Collections.Generic;
+
+
+    public class Cafetera
+    {
+        private int _saldo = 0;
+        private Dictionary<string, int> _inventario = new();
+
+        public int Saldo => _saldo;
+
+        public void InsertarMoneda(int monto)
+        {
+            // implementación mínima intencionalmente vacía para mantener tests en RED
+        }
+
+        public bool SeleccionarBebida(string nombre)
+        {
+            return true;
+        }
+
+        public void AgregarInventario(string nombre, int cantidad)
+        {
+            _inventario[nombre] = cantidad;
+        }
+
+        public int Inventario(string nombre)
+        {
+            return 0;
+        }
+
+        public int Cancelar()
+        {
+            int reembolso = 0;
+            _saldo = 0;
+            return reembolso;
+        }
+
+        public void ConfigurarExactChangeRequired(bool requerido) { }
+        public void SetModoMantenimiento(bool activo) { }
+        public void ConfigurarCambioDisponible(int cantidad) { }
+    }
+
